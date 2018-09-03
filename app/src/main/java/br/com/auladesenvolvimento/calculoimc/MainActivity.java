1a1cc8b28fd9a19071d89ae766cead2ac96be638
Arthur Luiz <tutsluiz@hotmail.com>
2018-09-02 21:57:37 -0300

    Comunicação do Java com o layout.

--- app/src/main/java/br/com/auladesenvolvimento/calculoimc/MainActivity.java
+++ app/src/main/java/br/com/auladesenvolvimento/calculoimc/MainActivity.java
@@ -2,11 +2,16 @@ package br.com.auladesenvolvimento.calculoimc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mainViewHolder = new ViewHolder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+        this.mainViewHolder.imc = (TextView) findViewById(R.id.imc);
+        this.mainViewHolder.classificacao = (TextView) findViewById(R.id.classificacao);
+        this.mainViewHolder.nome = (EditText) findViewById(R.id.nome);
+        this.mainViewHolder.peso = (EditText) findViewById(R.id.peso);
+        this.mainViewHolder.altura = (EditText) findViewById(R.id.altura);
+        this.mainViewHolder.calcular = (Button) findViewById(R.id.calcular);
 
 
     }
@@ -22,10 +33,39 @@ public class MainActivity extends AppCompatActivity {
     private class ViewHolder{
         TextView imc;
         TextView classificacao;
-        EditText name;
+        EditText nome;
         EditText peso;
         EditText altura;
         Button calcular;
+    }
+
+    public void Calcular(View view){
+        Double peso = Double.valueOf(this.mainViewHolder.peso.getText().toString());
+        Double altura = Double.valueOf(this.mainViewHolder.altura.getText().toString());
+        Double imc = calcularIMC(peso, altura);
+        String classificacao = classificarIMC(imc);
+        this.mainViewHolder.imc.setText(String.format(Locale.US, "%.2f", imc));
+        this.mainViewHolder.classificacao.setText(classificacao);
+    }
+
+    public Double calcularIMC(Double peso, Double altura){
+        return peso/(altura * altura);
+    }
+
+    public String classificarIMC(Double imc){
+        if(imc < 17)
+            return "Muito abaixo do Peso";
+        if(imc < 18.49)
+            return "Abaixo do Peso";
+        if(imc < 24.99)
+            return "Peso Normal";
+        if(imc < 29.99)
+            return "Acima do Peso";
+        if(imc < 34.99)
+            return "Obesidade I";
+        if(imc < 39.99)
+            return "Obsesidade II - Severa";
+        return "Obesidade III - Morbida";
     }
 }
 
